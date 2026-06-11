dd46d722e885a38cedbadd36c2b1407827e21a43
Barney Hsiao <2695818+avocadorice@users.noreply.github.com>
2026-06-11 14:33:57 -0700

    Configure game as a fully compliant Progressive Web App with Android TV Leanback Banner assets

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ellens-adventure-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './assets.js',
+  './levels.js',
+  './game.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './tv_banner.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
